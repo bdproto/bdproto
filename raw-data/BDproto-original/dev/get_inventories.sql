166f77041548f353098aaee0bcdeb1fae6e79408
bambooforest@gmail.com <bambooforest@gmail.com>
2018-04-12 11:21:52 +0200

    restructure and updated code for release

--- raw-data/BDproto-original/dev/get_inventories.sql
+++
@@ -1,6 +0,0 @@
-SELECT * 
-FROM BDPROTO.languesbdproto
-JOIN BDPROTO.segments
-ON BDPROTO.languesbdproto.Code = BDPROTO.segments.code
-
-# need to flip unicode2ipa.ipala (e.g. 2001) to unicode2ipa.unicode (hex)
